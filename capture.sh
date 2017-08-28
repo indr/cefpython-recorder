07d1ed9f60cfc6bedf2fa53bd7dec01e2dab3183
indr <mail@indr.ch>
2017-08-28 15:50:50 +0200

    Add clock.html

--- capture.sh
+++ capture.sh
@@ -5,5 +5,8 @@ then
   mkfifo fifo.rgba
 fi
 
-python capture.py http://localhost:8082/index.html 320 240 25
+# python capture.py http://localhost:8082/clock.html 320 240 25
+# python capture.py http://localhost:8082/fps.html 320 240 50
+# python capture.py https://threejs.org/examples/webgl_points_dynamic.html 320 240 25
+python capture.py http://www.indr.ch/ 320 240 25
 
